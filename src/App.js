b12b761e5ef063f5f9058338483743832289df59
Orlando Batres <201212734@ingenieria.usac.edu.gt>
2020-10-10 04:11:07 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Tab,Tabs} from 'react-bootstrap';
+import ServerRam from './components/ServerRam/ServerRam.js';
+import ServerCPU from './components/ServerCPU/ServerCPU.js';
+import ServerPublicaciones from './components/ServerPublicaciones/ServerPublicaciones.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid m-4">
+          <Tabs defaultActiveKey="servidorA" id="uncontrolled-tab-example" className="nav-fill">
+            <Tab eventKey="servidorA" title="Servidor A">
+              Server A
+              <ServerRam URL={'http://35.229.79.84:5001'}>
+
+              </ServerRam>
+              <ServerCPU URL={'http://35.229.79.84:5001'}>
+
+              </ServerCPU>
+              <ServerPublicaciones URL={'http://35.229.79.84:5001'}>
+
+              </ServerPublicaciones>
+            </Tab>
+            <Tab eventKey="profile" title="Servidor B">
+              Server B
+              <ServerRam URL={'http://35.184.149.249:5001'}>
+
+              </ServerRam>
+              <ServerCPU URL={'http://35.184.149.249:5001'}>
+
+              </ServerCPU>
+              <ServerPublicaciones URL={'http://35.184.149.249:5001'}>
+
+              </ServerPublicaciones>
+            </Tab>
+          </Tabs>
+        </div>
     </div>
   );
 }
